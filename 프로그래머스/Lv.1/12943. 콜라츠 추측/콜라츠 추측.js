277ee47c26c35d8b192460e3cf85df4fe205481c
2njeong <154481757+2njeong@users.noreply.github.com>
2024-01-15 09:49:54 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,20 @@
+function solution(n) {
+  var count = 0;
+
+  if (n === 1) {
+    return 0;
+  }
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = n * 3 + 1;
+    }
+
+    count++;
+  }
+  if (count > 500) {
+    return -1;
+  }
+  return count;
+}
