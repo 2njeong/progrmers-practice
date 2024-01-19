d6a1e68ed1e80c9364d5f039e8a20f6e4b86815e
2njeong <154481757+2njeong@users.noreply.github.com>
2024-01-19 09:35:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            arr[i] = "수";
+        } else {
+            arr[i] = "박";
+        }
+    }
+    return arr.join("");
+}
