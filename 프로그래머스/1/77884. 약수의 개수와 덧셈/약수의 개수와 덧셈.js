ebc4f891872751d75201f5cd41ec9194f8d53507
2njeong <154481757+2njeong@users.noreply.github.com>
2024-01-22 13:25:59 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,17 @@
+function solution(left, right) {
+  let sum = 0;
+  for (let num = left; num <= right; num++) {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) {
+        count++;
+      }
+    }
+    if (count % 2 === 0) {
+      sum += num;
+    } else {
+      sum -= num;
+    }
+  }
+  return sum;
+}
