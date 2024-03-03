dbf042f10d6ac43c28b36cfe30f581dc8eccee79
2njeong <154481757+2njeong@users.noreply.github.com>
2024-03-03 13:35:26 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,23 +1,21 @@
 function solution(nums) {
-  let arr = [];
+let arr = [];
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        arr.push(nums[i] + nums[j] + nums[k]);
-      }
+for (let i = 0; i < nums.length - 2; i++) {
+  for (let j = i + 1; j < nums.length - 1; j++) {
+    for (let k = j + 1; k < nums.length; k++) {
+      arr.push(nums[i] + nums[j] + nums[k]);
     }
   }
-  // console.log(arr);
+}
 
-  const dapArr = arr.filter((num) => {
-    let some = [];
-    for (let l = 2; l < num; l++) {
-      if (num % l === 0) {
-        some.push(l);
-      }
+const dapArr = arr.filter((num) => {
+  for (let l = 2; l < num; l++) {
+    if (num % l === 0) {
+      return false;
     }
-    return some.length === 0;
-  });
-  return dapArr.length;
+  }
+  return true;
+});
+return dapArr.length;
 }
