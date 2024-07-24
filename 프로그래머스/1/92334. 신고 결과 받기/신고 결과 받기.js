fcd2a6cc585d93e47548a3980398cc020e266629
2njeong <154481757+2njeong@users.noreply.github.com>
2024-07-24 23:51:26 +0900

    [level 1] Title: 신고 결과 받기, Time: 284.03 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,55 +1,51 @@
 function solution(id_list, report, k) {
-const 신고대장 = new Map();
-const 신고받이대장 = new Map();
+  const 신고대장 = new Map();
+  const 신고받이대장 = new Map();
+  const reports = [...new Set(report)].map((re) => re.split(" "));
 
-report.forEach((re) => {
-  const singleReport = re.split(" ");
-  const 신고자 = singleReport[0];
-  const 신고받은자 = singleReport[1];
+  reports.forEach((re) => {
+    const 신고자 = re[0];
+    const 신고받은자 = re[1];
 
-  const alreadyComplainedMap = 신고대장.get(신고자); // 이미 신고한 이력이 있는지
-  if (alreadyComplainedMap) {
-    if (!alreadyComplainedMap.get(신고받은자)) {
-      // 특정사람을 처음 신고하는 경우
-      alreadyComplainedMap.set(신고받은자, 1);
-      신고받이대장.get(신고받은자)
-        ? 신고받이대장.set(신고받은자, 신고받이대장.get(신고받은자) + 1)
-        : 신고받이대장.set(신고받은자, 1);
+    const alreadyComplainedMap = 신고대장.get(신고자); // 이미 신고한 이력이 있는지
+    if (alreadyComplainedMap) {
+      if (!alreadyComplainedMap.get(신고받은자)) {
+        // 특정사람을 처음 신고하는 경우
+        alreadyComplainedMap.set(신고받은자, 1);
+        신고받이대장.set(신고받은자, 신고받이대장.get(신고받은자) + 1 || 1);
+      }
+    } else {
+      // 신고자체를 처음하는 경우
+      const 개인신고대장 = new Map();
+      개인신고대장.set(신고받은자, 1);
+      신고대장.set(신고자, 개인신고대장);
+      신고받이대장.set(신고받은자, 신고받이대장.get(신고받은자) + 1 || 1);
     }
-  } else {
-    // 신고자체를 처음하는 경우
-    const 개인신고대장 = new Map();
-    개인신고대장.set(신고받은자, 1);
-    신고대장.set(신고자, 개인신고대장);
-    신고받이대장.get(신고받은자)
-      ? 신고받이대장.set(신고받은자, 신고받이대장.get(신고받은자) + 1)
-      : 신고받이대장.set(신고받은자, 1);
-  }
-});
-for (let [신고받은자, 신고받은카운트] of 신고받이대장) {
-  if (신고받은카운트 >= k) {
-    for (let [신고자, value] of 신고대장) {
+  });
+
+  reports.forEach((re) => {
+    const 신고자 = re[0];
+    const 신고받은자 = re[1];
+    if (신고받이대장.get(신고받은자) >= k) {
+      const value = 신고대장.get(신고자);
       if (value.get(신고받은자)) {
         const alreayGetMail = value.get("메일받는횟수");
-        alreayGetMail
-          ? value.set("메일받는횟수", alreayGetMail + 1)
-          : value.set("메일받는횟수", 1);
-        신고대장.set(신고자, value);
+        value.set("메일받는횟수", alreayGetMail + 1 || 1);
       }
+      신고대장.set(신고자, value);
     }
-  }
-}
+  });
 
-const answer = id_list.reduce((acc, cur) => {
-  acc.push(
-    신고대장.get(cur)
-      ? 신고대장.get(cur).get("메일받는횟수")
+  const answer = id_list.reduce((acc, cur) => {
+    acc.push(
+      신고대장.get(cur)
         ? 신고대장.get(cur).get("메일받는횟수")
+          ? 신고대장.get(cur).get("메일받는횟수")
+          : 0
         : 0
-      : 0
-  );
-  return acc;
-}, []);
+    );
+    return acc;
+  }, []);
 
-return answer;
+  return answer;
 }
